0dcd2e3c2689966bb8db0589962214a3e7c71332
Francesco Varano <shawill@trmpln.com>
2017-12-15 00:11:07 +0100

    PUT almost done

--- KiotlogAPI/webparts/Devices.fs
+++ KiotlogAPI/webparts/Devices.fs
@@ -6,8 +6,11 @@ open Kiotlog.Web.RestFul
 open Kiotlog.Web.DB
 open Kiotlog.Web.Railway
 
-open Microsoft.EntityFrameworkCore
 open KiotlogDB
+open Microsoft.EntityFrameworkCore
+// open Microsoft.FSharp.Linq
+// open System.Linq
+// open Kiotlog.Web.Webparts
 
 
 let getDevicesAsync (cs : string) () =
@@ -26,7 +29,6 @@ let getDevices (cs : string) () =
 
 let createDevice (cs : string) (device: Devices) =
     let ctx = getContext cs
-    // TODO: validate
 
     device |> ctx.Devices.Add |> ignore
 
@@ -42,15 +44,30 @@ let getDeviceAsync (cs : string) (deviceId : Guid) =
         use ctx = getContext cs
 
         try
-            let! device = ctx.Devices
-                            // .FindAsync(deviceId)
-                            .Include("Sensors")
-                            .Include("Sensors.SensorType")
-                            .SingleOrDefaultAsync(fun x -> x.Id.Equals(deviceId))
-                            |> Async.AwaitTask
+            // let! device = ctx.Devices
+            //                 // .FindAsync(deviceId)
+            //                 .Where(fun d -> d.Id.Equals(deviceId))                            
+            //                 .Include("Sensors.SensorType")
+            //                 // .Include(fun d -> d.Sensors)
+            //                 // .ThenInclude(fun s -> s.SensorType)
+            //                 // .SingleOrDefaultAsync(fun x -> x.Id.Equals(deviceId))
+            //                 .SingleOrDefaultAsync()
+            //                 |> Async.AwaitTask
+
+            // version of query using C# helpers
+            // let! device = Helpers.loadDeviceAsync(ctx, deviceId) |> Async.AwaitTask
+
+            let devices = ctx.Devices.Include("Sensors.SensorType")
+            let q =
+                query {
+                    for d in devices do
+                    where (d.Id = deviceId)
+                    select d
+                }
+            let! device = q.SingleOrDefaultAsync () |> Async.AwaitTask
 
             match device with
-            | null -> return Error { Errors = [|"Device not found"|]; Status = HTTP_404}
+            | null -> return Error { Errors = [|"Device not found"|]; Status = HTTP_404 }
             | d -> return Ok d
         with
         | _ -> return Error { Errors = [|"Some DB error occurred"|]; Status = HTTP_500 }
@@ -60,20 +77,51 @@ let getDevice (cs : string) (deviceId: Guid) =
     getDeviceAsync cs deviceId |> Async.RunSynchronously
     // use ctx = getContext cs
     // let devices = ctx.Devices
-    //                 .Include("Sensors")
+    //                 // .Include(fun d -> d.Sensors)
+    //                 // .ThenInclude(fun s -> s.SensorType)
     //                 .Include("Sensors.SensorType")
     // let q = query {
     //             for d in devices do
     //             where (d.Id = deviceId)
     //             select d
-    //             exactlyOneOrDefault
+    //             exactlyOneOrNone
     //         }
     // match q with
-    // | null -> None
-    // | d -> Some d
+    // | None -> Error { Errors = [|"Device not found"|]; Status = HTTP_404 }
+    // | Some d -> Ok d
 
-let updateDeviceById (deviceId: Guid) (device: Devices) =
-    Ok device
+let updateDeviceByIdAsync (cs : string) (deviceId: Guid) (device: Devices) =
+    async {
+        use ctx = getContext cs
+
+        device.Id <- deviceId
+
+        // // device |> ctx.Devices.Update |> ignore
+        // device |> ctx.Devices.Attach |> ignore
+
+        // // ctx.Entry(device).Property("Device").IsModified <- true
+
+        let! entity = ctx.Devices.FindAsync(deviceId) |> Async.AwaitTask
+
+        match entity with
+        | null -> return Error { Errors = [|"Device not found"|]; Status = HTTP_404}
+        | d ->
+            // d |> ctx.Devices.Remove |> ignore
+            if not (String.IsNullOrEmpty device.Device) then entity.Device <- device.Device
+            if not (isNull device.Auth) then entity.Auth <- device.Auth
+            if not (isNull device.Frame) then entity.Frame <- entity.Frame
+            if not (isNull device.Sensors) && device.Sensors.Count > 0 then entity.Sensors <- device.Sensors
+
+            try
+                ctx.SaveChanges() |> ignore
+                return Ok device
+            with
+            | :? DbUpdateException -> return Error { Errors = [|"Error updating Device"|]; Status = HTTP_409 }
+            | _ -> return Error { Errors = [|"Some DB error occurred"|]; Status = HTTP_500 }
+    }
+
+let updateDeviceById (cs : string) (deviceId: Guid) (device: Devices) =
+    updateDeviceByIdAsync cs deviceId device |> Async.RunSynchronously
 
 let deleteDeviceAsync (cs : string) (deviceId : Guid) =
     async {
@@ -106,7 +154,7 @@ let webPart (cs : string) =
             //Update = Db_Dictionary.updatePerson
             Delete = deleteDevice cs
             GetById = getDevice cs
-            UpdateById = updateDeviceById
+            UpdateById = updateDeviceById cs
             //IsExists = Db_Dictionary.isPersonExists
         }
     ]
