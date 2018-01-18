77ded658128f7b01f9ac9c741aaf0856cf3c4fce
Giampaolo Mancini <giampaolo@trampolineup.com>
2018-01-18 15:58:28 +0100

    Add SensorTypes endpoint update

--- KiotlogAPI/src/webparts/SensorTypes.fs
+++ KiotlogAPI/src/webparts/SensorTypes.fs
@@ -9,7 +9,6 @@ open Kiotlog.Web.Railway
 open KiotlogDB
 open Microsoft.EntityFrameworkCore
 
-
 let getSensorTypesAsync (cs : string) () =
     async {
         use ctx = getContext cs
@@ -36,10 +35,10 @@ let createSensorType (cs : string) (sensortype: SensorTypes) =
     | :? DbUpdateException -> Error { Errors = [|"Error adding SensorType"|]; Status = HTTP_409 }
     | _ -> Error { Errors = [|"Some DB error occurred"|]; Status = HTTP_500 }
 
-let private loadSensorTypeWithSensorsAsync (ctx : KiotlogDBContext) (sensortypeId : Guid) =
+let private loadSensorTypeAsync (ctx : KiotlogDBContext) (sensortypeId : Guid) =
     async {
         try
-            let sensortypes = ctx.SensorTypes.Include("Sensors")
+            let sensortypes = ctx.SensorTypes
             let q =
                 query {
                     for st in sensortypes do
@@ -53,63 +52,33 @@ let private loadSensorTypeWithSensorsAsync (ctx : KiotlogDBContext) (sensortypeI
             | d -> return Ok d
         with
         | _ -> return Error { Errors = [|"Some DB error occurred"|]; Status = HTTP_500 }
+
     }
 
 let getSensorTypeAsync (cs : string) (sensortypeId : Guid) =
     async {
         use ctx = getContext cs
 
-        return! loadSensorTypeWithSensorsAsync ctx sensortypeId
+        return! loadSensorTypeAsync ctx sensortypeId
     }
 
 let getSensorType (cs : string) (sensortypeId: Guid) =
     getSensorTypeAsync cs sensortypeId |> Async.RunSynchronously
 
-(*
 let updateSensorTypeByIdAsync (cs : string) (sensortypeId: Guid) (sensortype: SensorTypes) =
     async {
         use ctx = getContext cs
 
         sensortype.Id <- sensortypeId
 
-        let! res = loadSensorTypeWithSensorsAsync ctx sensortypeId
+        let! res = loadSensorTypeAsync ctx sensortypeId
 
         match res with
         | Error _ -> return res
         | Ok entity ->
-            // d |> ctx.SensorTypes.Remove |> ignore
-            if not (String.IsNullOrEmpty sensortype.SensorType) then entity.SensorType <- sensortype.SensorType
-            if not (isNull sensortype.Auth) then entity.Auth <- sensortype.Auth
-            if not (isNull sensortype.Frame) then entity.Frame <- entity.Frame
-
-            if not (isNull sensortype.Sensors) && sensortype.Sensors.Count > 0 then
-                let updateSensor = fun (s : Sensors) ->
-                    let existing =
-                        let f =
-                            query {
-                                for x in entity.Sensors do
-                                where (x.Id = s.Id)
-                                select x
-                                exactlyOneOrDefault
-                            } // entity.Sensors.SingleOrDefault(fun x -> x.Id = s.Id)
-                        match f with
-                        | null ->
-                            entity.Sensors.Add s
-                            s
-                        | _ -> f
-
-                    existing |> ctx.Sensors.Attach |> ignore
-                    if not (isNull s.Meta) then
-                        existing.Meta <- s.Meta
-                        // ctx.Entry(existing).Property("_Meta").IsModified <- true
-                    if s.ConversionId.HasValue then
-                        existing.ConversionId <- s.ConversionId
-                    if s.SensorTypeId.HasValue then
-                        existing.SensorTypeId <- s.SensorTypeId
-                    if not (isNull s.Fmt) then
-                        existing.Fmt <- s.Fmt
-
-                sensortype.Sensors |> Seq.iter updateSensor
+            if not (String.IsNullOrEmpty sensortype.Name) then entity.Name <- sensortype.Name
+            if not (isNull sensortype.Meta) then entity.Meta <- sensortype.Meta
+            if not (isNull sensortype.Type) then entity.Type <- entity.Type
 
             try
                 ctx.SaveChanges() |> ignore
@@ -120,9 +89,6 @@ let updateSensorTypeByIdAsync (cs : string) (sensortypeId: Guid) (sensortype: Se
     }
 let updateSensorTypeById (cs : string) (sensortypeId: Guid) (sensortype: SensorTypes) =
     updateSensorTypeByIdAsync cs sensortypeId sensortype |> Async.RunSynchronously
-*)
-let updateSensorTypeById (cs : string) (sensortypeId: Guid) (sensortype: SensorTypes) =
-    async { return Ok sensortype } |> Async.RunSynchronously
 
 let deleteSensorTypeAsync (cs : string) (sensortypeId : Guid) =
     async {
@@ -152,10 +118,8 @@ let webPart (cs : string) =
             Name = "sensortypes"
             GetAll = getSensorTypes cs
             Create = createSensorType cs
-            //Update = Db_Dictionary.updatePerson
             Delete = deleteSensorType cs
             GetById = getSensorType cs
             UpdateById = updateSensorTypeById cs
-            //IsExists = Db_Dictionary.isPersonExists
         }
     ]
