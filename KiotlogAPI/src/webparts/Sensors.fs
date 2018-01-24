22553f9b533f0e39451c9e9c31745599fd90c0d1
Giampaolo Mancini <giampaolo@trampolineup.com>
2018-01-24 17:32:19 +0100

    Remove unneeded wrappers

--- KiotlogAPI/src/webparts/Sensors.fs
+++ KiotlogAPI/src/webparts/Sensors.fs
@@ -7,35 +7,23 @@ open Kiotlog.Web.Webparts.Generics
 open Kiotlog.Web.RestFul
 open KiotlogDB
 
-let getSensors (cs : string) =
-    getEntities<Sensors> cs
-
-let createSensor (cs : string) (sensor : Sensors) =
-    createEntity<Sensors> cs sensor
-
-let getSensor (cs : string) (sensorId: Guid) =
-    getEntity<Sensors> cs sensorId
-
 let updateSensorById<'T when 'T : not struct and 'T : null> (cs : string) (sensorId: Guid) (sensor: Sensors) =
     let updateFunc (entity : Sensors) =
-             if not (isNull sensor.Meta) then entity.Meta <- sensor.Meta
-             if not (isNull sensor.Fmt) then entity.Fmt <- sensor.Fmt
-             if sensor.ConversionId.HasValue then entity.ConversionId <- sensor.ConversionId
-             if sensor.SensorTypeId.HasValue then entity.SensorTypeId <- sensor.SensorTypeId
+         if not (isNull sensor.Meta) then entity.Meta <- sensor.Meta
+         if not (isNull sensor.Fmt) then entity.Fmt <- sensor.Fmt
+         if sensor.ConversionId.HasValue then entity.ConversionId <- sensor.ConversionId
+         if sensor.SensorTypeId.HasValue then entity.SensorTypeId <- sensor.SensorTypeId
 
     updateEntityById<Sensors> cs sensorId updateFunc
-
-let deleteSensor (cs : string) (sensorId : Guid) =
-    deleteEntity<Sensors> cs sensorId
 
 let webPart (cs : string) =
     choose [
         rest {
             Name = "sensors"
-            GetAll = getSensors cs
-            Create = createSensor cs
-            Delete = deleteSensor cs
-            GetById = getSensor cs
+            GetAll = getEntities<Sensors> cs
+            Create = createEntity<Sensors> cs
+            Delete = deleteEntity<Sensors> cs
+            GetById = getEntity<Sensors> cs []
             UpdateById = updateSensorById cs
         }
     ]
