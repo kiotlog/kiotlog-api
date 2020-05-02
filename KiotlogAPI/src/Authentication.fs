0261f6e8492ad3ff71f2641bdb2aed259ea5cdd5
Francesco Varano <shawill@trmpln.com>
2020-05-02 10:23:28 +0200

    token from query string too

--- KiotlogAPI/src/Authentication.fs
+++ KiotlogAPI/src/Authentication.fs
@@ -5,7 +5,7 @@ open Suave.RequestErrors
 
 // let apiKey = "" // "83a1ab6a-52a5-48f5-836d-79662722345b"
 
-let internal parseAuthenticationToken (authorization : string) =
+let internal parseAuthorizationHeader (authorization : string) =
   let parts = authorization.Split (' ')
   match parts.Length with
   | 2 ->
@@ -15,10 +15,21 @@ let internal parseAuthenticationToken (authorization : string) =
       | _ -> Error "No Bearer Token"
   | _ -> Error "Wrong format for Authorization Header (need: Authorization: Bearer <token>)"
 
-let internal getToken (request: HttpRequest) =
+let internal getTokenFromHeader (request: HttpRequest) =
     match request.header "authorization" with
-    | Choice1Of2 header -> parseAuthenticationToken header
+    | Choice1Of2 header -> parseAuthorizationHeader header
     | Choice2Of2 _ -> Error "No Authorization Header"
+
+let internal getTokenFromQuery (request: HttpRequest) =
+    match request.queryParam "apiKey" with
+    | Choice1Of2 apiKey -> Ok apiKey
+    | Choice2Of2 _ -> Error "No apiKey in query string"
+
+let internal getToken (request: HttpRequest) =
+    match getTokenFromHeader request, getTokenFromQuery request with
+    | Ok token, Ok _ -> Ok token // prefer Bearer token
+    | Ok token, _ | _, Ok token -> Ok token
+    | _ -> Error "No API authentication token found"
 
 let internal checkToken (apiKey: string) (token: string) =
     if token = apiKey then
